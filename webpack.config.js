421a7299c5a6dfefb02f54acd58733ffde31a969
bushra-alam <siba742002@gmail.com>
2025-03-22 17:00:00 +0500

    "Installed webpack successfully "

---
+++ webpack.config.js
@@ -0,0 +1,42 @@
+import HtmlWebpackPlugin from 'html-webpack-plugin';
+import path from 'path';
+
+export default {
+  mode: 'development',
+  watch: false,
+  entry: './src/index.js',
+  output: {
+    filename: '[name].bundle.js',
+    path: path.resolve(process.cwd(), 'dist'),
+  },
+  devtool: 'inline-source-map',
+  devServer: {
+    static: './dist',
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      title: 'Development',
+    }),
+  ],
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: ['@babel/preset-env'],
+          },
+        },
+      },
+      {
+        test: /\.css$/i,
+        use: ['style-loader', 'css-loader'],
+      },
+    ],
+  },
+  optimization: {
+    runtimeChunk: 'single',
+  },
+};
